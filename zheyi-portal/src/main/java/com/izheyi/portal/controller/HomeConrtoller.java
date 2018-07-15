23d67db5f5efa7f3374ab9da05ad3967e9b75e85
Yongfei Hu <yongfei.hu@everbridge.com>
2018-07-15 12:24:36 +0800

    add portal project

---
+++ zheyi-portal/src/main/java/com/izheyi/portal/controller/HomeConrtoller.java
@@ -0,0 +1,15 @@
+package com.izheyi.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeConrtoller {
+	
+	@RequestMapping("/index")
+	public String home(){
+		return "index";
+	}
+	
+
+}
